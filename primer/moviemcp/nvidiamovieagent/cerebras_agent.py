1763497e5d726ffc9c351f63821f5485b31cba28
aikonstruktor <aikonstruktor@outlook.com>
2026-05-28 11:59:55 +0530

    Add WebSafeLiteLlm class and root_agent instance
    Introduced a WebSafeLiteLlm class to safely wrap LiteLlm, preventing crashes during serialization in dev-server. Created a root_agent instance using the new class.

---
+++ primer/moviemcp/nvidiamovieagent/cerebras_agent.py
@@ -0,0 +1,22 @@
+from typing import Any, Dict
+
+from google.adk.agents import Agent
+from google.adk.models.lite_llm import LiteLlm
+
+class WebSafeLiteLlm(LiteLlm):
+    """
+    Dev-Server-safe wrapper around LiteLlm.
+    Overrides serialization to prevent `adk web` from crashing on llm_client.
+    """
+    def model_dump(self, *args, **kwargs) -> Dict[str, Any]:
+        return {
+            "model": self.model,
+            "type": "LiteLlm (Web Safe Workaround)"
+        }
+
+root_agent = Agent(
+    name="cerebras_assistant",
+    # model=LiteLlm(model="huggingface/mistralai/Mistral-7B-Instruct-v0.2"),
+    model=WebSafeLiteLlm(model="cerebras/llama3.1-8b"),
+    instruction="You are a helpful assistant, that answers user questions in minimal tokens."
+)
